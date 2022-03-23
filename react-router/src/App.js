ff5f0020704d83fd26cee38c4ad947bcd2076068
Araz Shahinian <arshahinian@gmail.com>
2022-03-22 21:07:11 -0400

    Done

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import {BrowserRouter as Vector, Link,Routes, Route} from 'react-router-dom'
 
 import Home from './components/Home'
 import About from './components/About'
@@ -8,28 +9,38 @@ import Packages from './components/Packages'
 
 function App() {
 
-  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
+  const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath','Turkey Trot']
 
   return (
     <div className="App">
-      <header>
-        <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+      
+      <Vector>
+        <header>
+          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+          <div className="navBar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/packages">Our Packages</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
 
-        <div className="navBar">
-          <ul>
-            <li>
-              <a href={<Home />}>Home</a>
-            </li>
-            <li>
-              <a href={<About />}>About Us</a>
-            </li>
-            <li>
-              <a href={<Packages packages={packages}/>}>Our Packages</a>
-            </li>
-          </ul>
+        <div className="display" >
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/packages" element={<Packages packages={packages} />} />              
+            </Routes>
         </div>
 
-      </header>
+        </Vector>        
     </div>
   );
 }
